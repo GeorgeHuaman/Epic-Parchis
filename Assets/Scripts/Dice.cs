fe33ec71773d06a3522b4eaff049840feca48305
George Jimy <knivespeans@gmail.com>
2025-03-06 15:21:41 -0500

    Dice

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,62 @@
+using SpatialSys.UnitySDK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    private const int DICE_FACES = 6;
+
+    [Header("Dice Animation")]
+    [SerializeField] private Quaternion startingRotation;
+    [SerializeField] private Vector3 diceRotationShift;
+    [SerializeField] private float animationDuration;
+    [SerializeField] private float animationTimer;
+    [SerializeField] private bool isRolling;
+
+    [Header("Dice Positions")]
+    [SerializeField] private Vector3[] orientations;
+
+    private void Start()
+    {
+        startingRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        if(animationTimer > 0) 
+            animationTimer -= Time.deltaTime;
+
+        else if (isRolling)
+            EndRoll();
+
+        if (isRolling)
+            RollAnimation();
+    }
+
+    public void Roll()
+    {
+        if (isRolling) return;
+
+        isRolling = true;
+        animationTimer = animationDuration;
+    }
+
+    private void EndRoll()
+    {
+        isRolling = false;
+
+        int value = Random.Range(1, DICE_FACES + 1);
+        //SpatialBridge.coreGUIService.DisplayToastMessage(value.ToString());
+
+        if (orientations.Length > value - 1)
+            transform.rotation = Quaternion.Euler(orientations[value - 1]);
+        else 
+            transform.rotation = startingRotation;
+    }
+
+    private void RollAnimation()
+    {
+        transform.rotation *= Quaternion.Euler(diceRotationShift * Time.deltaTime);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,16 +7,16 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        GameObject[] player = GameObject.FindGameObjectsWithTag("Capsule");
-        for (int i = 0; i < player.Length; i++)
-        {
-            {
-                player[i].AddComponent<PlayerID>().id = i;
-                Debug.Log(i);
-            }
-        }
+        //GameObject[] player = GameObject.FindGameObjectsWithTag("Capsule");
+        //for (int i = 0; i < player.Length; i++)
+        //{
+        //    {
+        //        player[i].AddComponent<PlayerID>().id = i;
+        //        Debug.Log(i);
+        //    }
+        //}
 
-        Debug.Log(player.Length);
+        //Debug.Log(player.Length);
 
     }
 }
